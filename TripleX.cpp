c6850e5d6383c2a88a16c1a889dc0a6c6813f475
kaydingx <kayding@yeah.net>
2022-01-26 18:39:07 +0800

    first commit

---
+++ TripleX.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout<<"YHou are a secret agent breaking into a secure sever room..."<<std::endl;
+    std::cout<<"Enter the correct code to continue..."<<std::endl;
+}
