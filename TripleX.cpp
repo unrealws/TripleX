b8a9bcdc7c1cd0b26cb0de51194d5ae1eb67a889
kaydingx <kayding@yeah.net>
2022-02-01 14:17:18 +0800

    ignore something

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,9 +1,18 @@
 
 #include <iostream>
 
-
 int main()
 {
     std::cout<<"YHou are a secret agent breaking into a secure sever room..."<<std::endl;
     std::cout<<"Enter the correct code to continue..."<<std::endl;
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
 }
