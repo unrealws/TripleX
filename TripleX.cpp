9de28e9565ab88c62cf81c7f6d6fee3901b6aa24
kaydingx <kayding@yeah.net>
2022-02-03 11:38:45 +0800

    add game loop and functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,15 @@
 
 #include <iostream>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
-    std::cout << "YHou are a secret agent breaking into a secure sever room..." << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    std::cout << "\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure sever room...\nEnter the correct code to continue..." << std::endl;
+}
 
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
     const int CodeA = 4;
     const int CodeB = 3;
     const int CodeC = 2;
@@ -13,14 +17,45 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
+    // Print CodeSUm and CodeProduct to the terminal
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    int PlayerGuess;
-    std::cin >> PlayerGuess;
-    std::cout << "You entered: " << PlayerGuess << std::endl;
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check if the player guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "You Win!" << std::endl;
+        return true;
+    }
+    else
+    {
+        std::cout << "You Lose!" << std::endl;
+        return false;
+    }
+}
+
+int main()
+{
+    int LevelDifficulty = 1;
+    while (true)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+           ++LevelDifficulty;
+        }
+    }
     return 0;
 }
